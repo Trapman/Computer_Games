b882e9b0525a5cb6a8833a0beb328a791055fdd6
Trapman <david.a.shuttleworth@gmail.com>
2019-06-20 16:05:28 -0400

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,7 @@
+#We will use lists, in, methods such as split(), lower(), upper(), startswith(), and endswith(), elif
+
+import random  #since we'll want to randomly select a secret word for the player to guess
+
+#build out constant variables for the hang man shapes. Basically a list of ASCII
+
+HANGMAN_PICS = 

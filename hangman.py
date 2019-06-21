7f93494017d00c49f74bb53d3cfa211a95504fe6
Trapman <david.a.shuttleworth@gmail.com>
2019-06-21 18:13:19 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@
 
 import random  #since we'll want to randomly select a secret word for the player to guess
 
-#build out constant variables for the hang man shapes. Basically a list of ASCII
+#make a constant variable with a list() for the hang man shapes (in ASCII)
 
 HANGMAN_PICS = ['''
   +---+
@@ -40,3 +40,11 @@ HANGMAN_PICS = ['''
  /|\  |
  /\   |
      ===''', ''']
+
+words = 'ant buford bulldog henley emi holly thai tut chase bradley dan tim felix zbo arlo'.split() #use split() here to make all of these words their own string instead of writing them all out manually like 'ant', 'buford', etc..
+
+def getRandomWord(wordList):
+  #this function returns a random string from the list of strings you pass into it.
+  wordIndex = random.randint(0, len(wordList) - 1)
+  return wordList(wordIndex)
+

7c6578abc37c9bb34edbbdb4c27727be6d31c72a
Trapman <david.a.shuttleworth@gmail.com>
2019-07-04 14:08:33 -0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -40,4 +40,15 @@ def makeMove(board, letter, move):
 def isWinner(bo, le):
   #given a board and a player's letter, this function returns True if that player has won.
   #we use 'bo' instead of 'board' and 'le' instead of 'letter' to cut down typing.
+  return ((bo[7] == le and bo[8] == le and bo[9] == le) or #Across the top
+          (bo[4] == le and bo[5] == le and bo[6] == le) or #Across the middle
+          (bo[1] == le and bo[2] == le and bo[3] == le) or #Across the bottom
+          (bo[7] == le and bo[4] == le and bo[1] == le) or #Down the left side
+          (bo[8] == le and bo[5] == le and bo[2] == le) or #Down the middle
+          (bo[9] == le and bo[6] == le and bo[3] == le) or #Down the right side
+          (bo[7] == le and bo[5] == le and bo[3] == le) or #Diagonal
+          (bo[9] == le and bo[5] == le and bo[1] == le) or #Diagonal 
+          
+
+          
   

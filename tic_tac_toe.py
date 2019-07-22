1becdf885620cb1b105fc2f13e61dd2739c01206
Trapman <david.a.shuttleworth@gmail.com>
2019-07-22 15:33:31 -0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,6 @@
 #build in basic AI to play against the player
+#need to finish explaining all of the different functions
+
 #the basic strucure of the flowchart:
 """program starts by asking player to choose X or O. Program then randomly chooses who goes first.
    Then the player and computer take turns making moves. After the player or the computer makes a move, the program

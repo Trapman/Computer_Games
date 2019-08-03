58b24dec675ec716704e96321311031c7694583c
Trapman <david.a.shuttleworth@gmail.com>
2019-08-03 17:20:14 -0400

    Update bagels.py

--- bagels.py
+++ bagels.py
@@ -21,4 +21,21 @@ def getClues(guess, secretNum):
   for i in range(len(guess)):
     if guess[i] == secretNum[i]:        #if it matches the exact position
       clues.append('Fermi')
-    elif guess[i]
+    elif guess[i] in secretNum:
+      clues.append('Pico')
+   if len(clues) == 0:
+      return 'Bagels'
+    
+clues.sort()
+return ' '.join(clues)
+
+def isOnlyDigits(num):
+  #returns True is num is a strong of only digits. Otherwise, returns False.
+  if num == '':
+    return False
+  
+  for i in num: 
+    if i not in '0 1 2 3 4 5 6 7 8 9'.split():
+      return False
+    
+    return True
